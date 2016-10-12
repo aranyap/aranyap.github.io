5b91479cee40c4c89e280e732233b0b1d9e049ec
aranyap <aranyaphookan@Aranyas-MacBook-Air.local>
2016-10-12 20:27:55 +1100

    today's work

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ function start() {
   $('.nav-item').hover(becomeActive, stopBeingActive);
   $('.slide-menu-item a').hover(becomeActive, stopBeingActive);
   $('#hamburger-btn').click(slideMenutoggle);
+  $('.pure-form').submit(validate);
+  $('#firstName').blur(runFirstNameValidation);
 }
 
 function becomeActive() {
@@ -17,4 +19,46 @@ function stopBeingActive() {
 function slideMenutoggle() {
   $('.slide-menu').toggleClass('open');
   $('#hamburger-btn').toggleClass('open');
+}
+
+function validate(e) {
+	if(firstNameValid() != true) {
+		firstNameError();
+	}
+	if(lastNameValid() != true) {
+		lastNameError();
+	}
+	if(firstNameValid() && lastNameValid()) {
+		alert('Thanks for entering!');
+	} else {
+	e.preventDefault();
+	}
+}
+
+function runFirstNameValidation() {
+	if(firstNameValid()) {
+		firstNameClearError();
+	}
+}
+
+
+function firstNameError() {
+	$('#firstName').addClass('error');
+	$('#firstName').siblings('.error-msg').show();
+}
+
+function firstNameError() {
+	$('#firstName').removeClass('error');
+	// $('#firstName').siblings('.error-msg').hide();
+}
+
+
+function firstNameValid() {
+	var firstName = $('#firstName').val();
+	return firstName.length > 0
+}
+
+function lastNameValid() {
+	var lastName = $('#lastName').val();
+	return lastName.length > 0
 }
